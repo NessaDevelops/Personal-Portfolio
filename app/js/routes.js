aeea6360660f13decdcf63a9bbea3b451fcc9bec
Nessa <nessa.develops@gmail.com>
2017-06-23 11:30:00 -0500

    Routing refresh working

--- app/js/routes.js
+++ app/js/routes.js
@@ -1,21 +1,19 @@
 var app = angular.module("myApp", ["ngRoute"]);
-app.config(function($routeProvider, $locationProvider) {
+app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl : "app/partials/home.html"
+        templateUrl : "/app/partials/home.html"
     })
     .when("/about", {
-        templateUrl : "app/partials/about.html"
+        templateUrl : "/app/partials/about.html"
     })
     .when("/projects", {
-        templateUrl : "app/partials/projects.html"
+        templateUrl : "/app/partials/projects.html"
     })
     .when("/contact", {
-        templateUrl : "app/partials/contact.html"
+        templateUrl : "/app/partials/contact.html"
     })
     .otherwise({
         redirectTo: '/'
     });
-    
-    $locationProvider.html5Mode(true);
 });
