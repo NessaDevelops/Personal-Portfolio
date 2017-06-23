d41d6ae852a780c8f8c41251249cdc5f99f9a7c0
Nessa <nessa.develops@gmail.com>
2017-06-23 16:57:44 -0500

    Form validation styling

--- app/js/form.js
+++ app/js/form.js
@@ -1,29 +1,29 @@
 $(document).ready(function() {
 
-    $('#contactForm').submit(function(e) {
-        e.preventDefault();
-        var name = $('#name').val();
-        var email = $('#email').val();
-        var message = $('#message').val();
+    // $('#contactForm').submit(function(e) {
+    //     e.preventDefault();
+    //     var name = $('#name').val();
+    //     var email = $('#email').val();
+    //     var message = $('#message').val();
 
-        $(".error").remove();
+    //     $(".error").remove();
 
-        if (name.length < 1) {
-            $('#name').after('<span class="error">This field is required</span>');
-        }
-        if (email.length < 1) {
-            $('#email').after('<span class="error">This field is required</span>');
-        } else {
-            var regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            var validEmail = regEx.test(email);
-            if (!validEmail) {
-                $('#email').after('<span class="error">Enter a valid email</span>');
-            }
-        }
-        if (message.length < 1) {
-            $('#message').after('<span class="error">This field is required</span>');
-        }
-    });
+    //     if (name.length < 1) {
+    //         $('#name').after('<span class="error">This field is required</span>');
+    //     }
+    //     if (email.length < 1) {
+    //         $('#email').after('<span class="error">This field is required</span>');
+    //     } else {
+    //         var regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    //         var validEmail = regEx.test(email);
+    //         if (!validEmail) {
+    //             $('#email').after('<span class="error">Enter a valid email</span>');
+    //         }
+    //     }
+    //     if (message.length < 1) {
+    //         $('#message').after('<span class="error">This field is required</span>');
+    //     }
+    // });
 
     // $('form[id="contactForm"]').validate({
     //     rules: {
